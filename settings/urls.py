332aea1174f176dd2b487addac3b1f723390862c
flomasta <zatevi@mail.ru>
2023-01-18 17:20:40 +0300

    updated ali_link

--- settings/urls.py
+++ settings/urls.py
@@ -1,6 +1,5 @@
-url_ali = 'http://aliexpress.ru/item/1005003727034635.html?sub=45ro61bcj7l9p1ez9udmhgh1merbqlnr&af=1954_3887060&utm_campaign=1954_3887060&aff_platform=api-new-link-generate&utm_medium=cpa&cn=20ruro61bc2joyevbdugjztrut6evlru&dp=20ruro61bc2joyevbdugjztrut6evlru&aff_fcid=67a9da0b9b84492999283e61906290f6-1673179176360-05019-_DcZkoMX&cv=2&aff_fsk=_DcZkoMX&sk=_DcZkoMX&aff_trace_key=67a9da0b9b84492999283e61906290f6-1673179176360-05019-_DcZkoMX&terminal_id=f5ff7ca6d7db4e10b6cc4e7c8d3ba427&utm_source=aerkol&utm_content=2&sku_id=12000026949934273'
+url_ali = 'https://aliexpress.ru/item/32710263385.html?sku_id=60908821645'
 
 url_cb = 'http://cbr.ru/currency_base/daily/'
 
 url_ali_alt = 'http://helpix.ru/currency/'
-
